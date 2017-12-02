6508e68c401305368714e43d4ca38a02c6ac7e75
David Poss <davidposs@csu.fullerton.edu>
2017-12-02 02:32:11 -0800

    cleaned up some code, still not working

--- recognizer.py
+++ recognizer.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """ Facial Recognition Program using University of Essex Dataset """
 
+from datetime import datetime
+import time
+import tempfile
 import os
 import cv2
-import time
 import tensorflow as tf
-from datetime import datetime
 
 EPOCHS = 10
-BATCH_SIZE = 8
+BATCH_SIZE = 50
+# All images are sized at 180 x 180
 IMAGE_SIZE = (180, 180)
 TRAIN_PATH = "../../Data/NewTrain/"
 TEST_PATH = "../../Data/NewTest/"
@@ -35,9 +37,9 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-def conv2d(x, weights):
+def conv2d(x_image, weights):
     """ Single step convolution, allows output to be same size as input """
-    return tf.nn.conv2d(x, weights, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x_image, weights, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(matrix):
@@ -68,99 +70,118 @@ def encode_labels(train_labels, test_labels):
         found_labels.append(i)
     return [mapping[i] for i in train_labels], [mapping[i] for i in test_labels], mapping
 
+def predict(pred_image, y_conv, x_images, keep, sess):
+    """ Provided an input path, """
+    pred_image = "../../ardper.13.jpg"
+    prediction = tf.argmax(y_conv, 1)
+    flat = gray(resize(cv2.imread(pred_image))).flatten()
+    pred = prediction.eval(feed_dict={x_images: [flat], keep: 1.0}, session=sess)
+    return pred
+
 
 def main():
     """ Main CNN code """
     train_image_paths, train_labels = get_paths_and_labels(TRAIN_PATH)
     train_images = [gray(cv2.imread(image)).flatten() for image in train_image_paths]
     num_train_images = len(train_image_paths)
+    print("Num train images {}".format(num_train_images))
 
     test_image_paths, test_labels = get_paths_and_labels(TEST_PATH)
+    num_test_images = len(test_image_paths)
+    print("Num test images {}".format(num_test_images))
+
     test_images = [gray(cv2.imread(image)).flatten() for image in test_image_paths]
 
     num_classes = len(set(train_labels))
 
     # Placeholders
-    x = tf.placeholder(tf.float32, shape=[None, IMAGE_SIZE[0] * IMAGE_SIZE[1]])
-    y_ = tf.placeholder(tf.float32, shape=[None, num_classes])
-    x_image = tf.reshape(x, [-1, IMAGE_SIZE[0], IMAGE_SIZE[1], 1])
+    x_images = tf.placeholder(tf.float32, shape=[None, IMAGE_SIZE[0] * IMAGE_SIZE[1]])
+    y_labels = tf.placeholder(tf.float32, shape=[None, num_classes])
+
+    with tf.name_scope("Reshape"):
+        x_image = tf.reshape(x_images, [-1, IMAGE_SIZE[0], IMAGE_SIZE[1], 1])
 
     # One-hot labels
     train_labels, test_labels, mapping = encode_labels(train_labels, test_labels)
-
     train_labels = tf.one_hot(indices=tf.cast(train_labels, tf.int32), depth=num_classes)
     test_labels = tf.one_hot(indices=tf.cast(test_labels, tf.int32), depth=num_classes)
 
-    # Convolution 1
-    weight_conv1 = weight_variable([5, 5, 1, 64])
-    bias_conv1 = bias_variable([64])
-    h_conv1 = tf.nn.relu(conv2d(x_image, weight_conv1) + bias_conv1)
+    with tf.name_scope("Convolution1"):
+        weight_conv1 = weight_variable([5, 5, 1, 32])
+        bias_conv1 = bias_variable([32])
+        h_conv1 = tf.nn.relu(conv2d(x_image, weight_conv1) + bias_conv1)
 
-    # Pooling 1 : 180x180 -> 90x90
-    h_pool1 = max_pool_2x2(h_conv1)
+    with tf.name_scope("Pooling1"): # Pools 180x180 -> 90x90
+        h_pool1 = max_pool_2x2(h_conv1)
 
-    # Convolution 2
-    weight_conv2 = weight_variable([5, 5, 64, 32])
-    bias_conv2 = bias_variable([32])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, weight_conv2) + bias_conv2)
+    with tf.name_scope("Convolution2"):
+        weight_conv2 = weight_variable([5, 5, 32, 64])
+        bias_conv2 = bias_variable([64])
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, weight_conv2) + bias_conv2)
 
-    # Pooling 2 :  90x90 -> 45x45
-    h_pool2 = max_pool_2x2(h_conv2)
+    with tf.name_scope("Pooling2"): # Pools 90x90 -> 45x45
+        h_pool2 = max_pool_2x2(h_conv2)
 
-    # Fully connected layer 1
-    w_fc1 = weight_variable([45*45*32, 1024])
-    b_fc1 = bias_variable([1024])
-    h_pool2_flattened = tf.reshape(h_pool2, [-1, 45*45*32])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flattened, w_fc1) + b_fc1)
-    keep = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep)
+    with tf.name_scope("FC1"):
+        weight_fc1 = weight_variable([45*45*64, 1024])
+        bias_fc1 = bias_variable([1024])
+        h_pool2_flattened = tf.reshape(h_pool2, [-1, 45*45*64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flattened, weight_fc1) + bias_fc1)
 
-    # Fully Connected Layer 2
-    weight_fc2 = weight_variable([1024, num_classes])
-    bias_fc2 = bias_variable([num_classes])
-    y_conv = tf.matmul(h_fc1_drop, weight_fc2) + bias_fc2
+    with tf.name_scope("Dropout"):
+        keep = tf.placeholder(tf.float32)
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep)
 
-    cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    with tf.name_scope("FC2"):
+        weight_fc2 = weight_variable([1024, num_classes])
+        bias_fc2 = bias_variable([num_classes])
+        y_conv = tf.matmul(h_fc1_drop, weight_fc2) + bias_fc2
+
+    with tf.name_scope("CrossEntropy"):
+        cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=y_labels, logits=y_conv))
+
+    with tf.name_scope("Optimization"):
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+    with tf.name_scope("Accuracy"):
+        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_labels, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    graph_loc = tempfile.mkdtemp()
+    print("Saving graph to {}".format(graph_loc))
+    writer = tf.summary.FileWriter(graph_loc)
+    writer.add_graph(tf.get_default_graph())
 
     print("Training...")
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         train_labels = train_labels.eval()
+        test_labels = test_labels.eval()
         for i in range(EPOCHS):
             start = time.time()
-            print ("Epoch {} started at {}".format(i, datetime.now().time()))
+            print("Epoch {} started at {}".format(i, datetime.now().time()))
             for batch_num in range(num_train_images//BATCH_SIZE):
-                # Read images, convert to gray-scale, resize to 180x180 and flatten them
-                curr_batch = train_images[batch_num * BATCH_SIZE : (batch_num + 1) * BATCH_SIZE]
-                #curr_batch = [gray(image).flatten() for image in curr_batch]
-                # Get labels for current batch
-                curr_labels = train_labels[batch_num * BATCH_SIZE : (batch_num + 1) * BATCH_SIZE]
-                sess.run([train_step], feed_dict={x:curr_batch, y_:curr_labels, keep:0.5})
+                batch_images = train_images[batch_num * BATCH_SIZE : (batch_num + 1) * BATCH_SIZE]
+                batch_labels = train_labels[batch_num * BATCH_SIZE : (batch_num + 1) * BATCH_SIZE]
+                if i % 2 == 1:
+                    train_accuracy = accuracy.eval(
+                        feed_dict={x_images:batch_images, y_labels: batch_labels, keep: 1.0})
+                    print("Training accuracy : {}".format(train_accuracy))
+                train_step.run(
+                    feed_dict={x_images: batch_images, y_labels: batch_labels, keep: 0.5})
 
-            train_accuracy = accuracy.eval(feed_dict={x:curr_batch, y_: curr_labels, keep: 1.0})
-            print("training accuracy : {}".format(train_accuracy))
-            train_step.run(feed_dict={x: curr_batch, y_:curr_labels, keep: 0.5})
             end = time.time()
             print("Finished at {}, time elapsed {}\n\n".format(datetime.now().time(), end - start))
 
-        print("Final train accuracy {}".format(accuracy.eval(
-            feed_dict={x: curr_batch, y_: curr_labels, keep: 0.4})))
         # Run test set
-        test_labels = test_labels.eval()
-        #test_images =[gray(image).flatten() for image in test_images]
-        print("Test Accuracy {}".format(accuracy.eval(feed_dict={x:test_images, y_:test_labels, keep:1.0})))
+        test_accuracy = accuracy.eval(
+            feed_dict={x_images:test_images, y_labels:test_labels, keep:1.0})
+        print("Test Accuracy {}".format(test_accuracy))
 
         # Predict
-        pred_image = "../../ardper.13.jpg"
-        prediction = tf.argmax(y_conv, 1)
-        flat = gray(resize(cv2.imread(pred_image))).flatten()
-        pred = prediction.eval(feed_dict={x:[flat], keep:1.0}, session=sess)
-        print(pred)
-        print("Correct: {}".format(mapping['ardper']))
+        prediction = predict("../../ardper.13.jpg", y_conv, x_images, keep, sess)
+        print("Guessed {}, Correct {}".format(prediction, mapping['ardper']))
 
 
 if __name__ == "__main__":
