668aa48f5c6426f55e9323a6b6395deb3320d31e
david poss <davidposs@ip-172-31-5-29.us-west-1.compute.internal>
2017-12-04 09:42:22 +0000

    Server.py now accepts and rejects users accordingly, authenticate.py has been slightly adjusted

--- authenticate.py
+++ authenticate.py
@@ -7,7 +7,7 @@ def is_valid_user(username):
     """ Looks for the username client sends in list of users on machine """
     users = subprocess.Popen(["ls", "/home"], stdout=subprocess.PIPE).communicate()
     # Popen ls /home likes to return a blank string at the end
-    users = users[0].split("\n")[:-1]
+    users = users[0].decode().split("\n")[:-1]
     return username in users
 
 
