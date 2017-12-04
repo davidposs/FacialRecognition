fb04a3cb0f43300cd29c898bb89c1b649ed6690a
david poss <david@localhost.localdomain>
2017-12-04 01:35:14 -0800

    User can launch a remote session by sending an image in sender.py, added a preprocessing script, made recognizer.py friendlier

--- sender.py
+++ sender.py
@@ -1,16 +1,39 @@
-""" Program to send an image to a remote server (AWS EC2 instance) """
+"""
+Created on Thu Nov 16 16:27:29 2017
+@author: David Poss
 
-import sys
-import socket
-import re
+Program to send an image to a remote server (AWS EC2 instance)
+"""
+
 import os
+import cv2
+import socket
+import subprocess
 
 INDENT = " " * 4
 
 
+def valid_input(args):
+    """ Checks command line arguments for validity """
+    if len(args) == 1:
+        print("[!] Please supply an image to use")
+        return False
+    if len(args) > 2:
+        print("[!] Too many arguments, please only supply one")
+        return False
+    if not os.path.exists(args[1]):
+        print("[!] {} is not a valid file path".format(args[1]))
+        return False
+    if os.path.exists(args[1]) and args[1].endswith(".jpg"):
+        return True
+    return False
+
+
 def open_session(username, password, host):
     """ Opens a putty session on the client computer. Linux only format right now"""
-    os.system("putty -ssh -l {} {} -pw {}".format(username, host, password))
+    password = password.split("\n")[0]
+    subprocess.Popen(["gnome-terminal", "-e",
+                      "plink {}@{} -pw {}".format(username, host, password)])
 
 
 def connect_to_server(host, port):
@@ -55,19 +78,51 @@ def send_image(image_path, client_sock, size):
         return False
 
 
+def take_picture(username, out_type):
+    """ Takes a picture of the user to send to the server """
+    cascades = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    video_capture = cv2.VideoCapture(0)
+    print("Taking a picture to send to the server")
+    while True:
+        try:
+            _, frame = video_capture.read()
+            grayed = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = cascades.detectMultiScale(grayed, scaleFactor=1.1, minNeighbors=5)
+            # Get x, y coordinates and width and height of face
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+            cv2.imshow('Webcam', frame)
+
+            key_press = cv2.waitKey(1)
+            # Press 's' to save detected face
+            if key_press & 0xFF == ord('q'):
+                return False
+            elif key_press & 0xFF == ord('s'):
+                face_to_save = frame[y:y + h, x:x + w]
+                cropped_face = cv2.resize(face_to_save, (180, 180), interpolation=cv2.INTER_AREA)
+                cropped_face = cv2.cvtColor(cropped_face, cv2.COLOR_BGR2GRAY)
+                # Add a ".1" to keep file naming convention
+                image_path = username + "." + str(1) + out_type
+                cv2.imwrite(image_path, cropped_face)
+                return image_path
+        except KeyboardInterrupt:
+            print("[!] User stopped, quitting")
+            return None
+
+
 def main():
     """ Main program to send a label and image to the server to authenticate the user. On success
         the program will launch a putty ssh session to the server under the user's account """
     host = "54.215.129.201"
     port = 45400
-    image_path = sys.argv[1]
 
+    username = input("Enter your username: ")
+    image_path = take_picture(username, out_type=".jpg")
     if not image_path:
-        print("[!] {} is not a valid image path".format(image_path))
+        print("[!] Error taking the picture, quitting")
         return
-
     # Regular expression to grab the labels from the pathname
-    username = re.findall(r'[^/]*$', image_path)[0].split(".")[0]
+    # username = re.findall(r'[^/]*$', image_path)[0].split(".")[0]
     with open(image_path, "rb") as image:
         size = len(image.read())
 
@@ -76,13 +131,15 @@ def main():
     send_image_size(size, client_sock)
     send_image(image_path, client_sock, size)
 
-    print("[*] Waiting for server response...")
+    print("[*] Waiting for authentication...")
     password = client_sock.recv(1024).decode()
     if password:
-        print("{}[+] received credentials!".format(INDENT))
+        print("{}[+] Access granted!".format(INDENT))
         open_session(username, password, host)
-        client_sock.shutdown(socket.SHUT_WR)
-        client_sock.close()
+        try:
+            client_sock.close()
+        finally:
+            return
     else:
         print("{}[!] Could not get credentials".format(INDENT))
         client_sock.shutdown(socket.SHUT_WR)
